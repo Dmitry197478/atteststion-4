09572a2704150556e6fafcf4233cf59fbbbe4665
Dmitry <118543883+Dmitry197478@users.noreply.github.com>
2022-12-06 15:44:16 +0300

    Create task3.sql

---
+++ task3.sql
@@ -0,0 +1,10 @@
+--Задача 3
+--Найти самую часто встречающуюся локации среди всех заказчиков. Вывести: location, count.
+
+SELECT
+	location,
+    	COUNT(location) AS v_o
+FROM Customer
+    GROUP BY location
+    ORDER BY v_o DESC
+    LIMIT    1;
