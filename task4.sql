8eb54d0e006afb7b72e1b2fe7be0b2965ce1bb69
Dmitry <118543883+Dmitry197478@users.noreply.github.com>
2022-12-06 15:45:11 +0300

    Create task4.sql

---
+++ task4.sql
@@ -0,0 +1,10 @@
+--Задача 4
+--Найти контракты одинаковой стоимости. Вывести count, amount
+
+SELECT 
+	amount,
+	COUNT(amount) AS countt
+FROM Contract 
+	GROUP BY amount 
+    HAVING COUNT(amount)>1
+    ORDER BY countt DESC
